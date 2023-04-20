07fe87ffdc7e67658002361cb94f7ba628928b33
Mridul Koirala <mrkoirala58@gmail.com>
2023-04-20 14:47:26 +0545

    feed component base

--- thesis/pages/index.tsx
+++ thesis/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 function index() {
   return (
     <main className='bg-black min-h-screen'>
       <Sidebar/>
+      <Feed/>
     </main>
   )
 }
