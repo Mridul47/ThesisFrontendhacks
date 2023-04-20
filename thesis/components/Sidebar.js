bb7b44eaf86674bb5f4ac44ce7f9ec371d34b7ea
Mridul Koirala <mrkoirala58@gmail.com>
2023-04-20 14:23:04 +0545

    sidebar link in sidebar js

--- thesis/components/Sidebar.js
+++ thesis/components/Sidebar.js
@@ -10,6 +10,7 @@ import {
     EllipsisVerticalIcon,
     BellAlertIcon,
     ListBulletIcon,
+    EllipsisHorizontalIcon,
 
 } from "@heroicons/react/24/outline";
 
@@ -32,7 +33,18 @@ function Sidebar() {
             <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
                 Tweet
             </button>
-
+            <div className='text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-auto'>
+                <img
+                    src="https://pbs.twimg.com/profile_images/1634262876423852035/VG4YRyVr_400x400.jpg"
+                    alt=""
+                    className="h-10 w-10 rounded-full xl:mr-2.5"
+                />
+                <div className="hidden xl:inline leading-5">
+                    <h4 className="font-bold">mridul123</h4>
+                    <p className="text-[#6e767d]">@mridul123</p>
+                </div>
+                <EllipsisHorizontalIcon className="h-5 hidden xl:inline ml-10" />
+            </div>
         </div>
     )
 }
