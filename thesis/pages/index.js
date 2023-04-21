52f50e7e94547915819082584ac42b4c59809065
Mridul Koirala <mrkoirala58@gmail.com>
2023-04-21 20:35:39 +0545

    session related logic acquired

--- thesis/pages/index.js
+++ thesis/pages/index.js
@@ -2,9 +2,15 @@ import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
 import { getProviders, getSession, useSession } from "next-auth/react";
-import { GetServerSidePropsContext } from 'next';
+import Login from '../components/Login'
 
-export default function index() {
+
+export default function index({trendingResults, followResults, providers}) {
+  //session ko kaam k vanda login nagari homepage nadehauna lai ho
+  const { data: session } = useSession();
+
+  // ra yaha ko le tei edi login chaina vane login ko page dekhaune logic ho
+  if(!session) return <Login providers={providers}/>;
   return (
     <main className='bg-black min-h-screen'>
       <Sidebar/>
@@ -12,7 +18,7 @@ export default function index() {
     </main>
   )
 }
-export async function getServerSideProps(context:GetServerSidePropsContext) {
+export async function getServerSideProps(context) {
   const trendingResults = await fetch("https://jsonplaceholder.typicode.com/posts").then(
     (res) => res.json()
   );
