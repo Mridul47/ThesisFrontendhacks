2b5aa920ce25545e507f04d492102e63128a2289
Mridul Koirala <mrkoirala58@gmail.com>
2023-04-22 20:17:16 +0545

    like/unlike plus moment for the post added

--- thesis/components/Post.js
+++ thesis/components/Post.js
@@ -1,132 +1,166 @@
-import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
-import React, { useState } from "react";
+import { EllipsisHorizontalIcon, GiftIcon } from "@heroicons/react/24/solid";
+import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import {
-  ChartBarIcon,
-  ChatBubbleLeftRightIcon,
-  ShareIcon,
-  TrashIcon,
+    ChartBarIcon,
+    ChatBubbleLeftRightIcon,
+    GiftTopIcon,
+    ShareIcon,
+    TrashIcon,
 } from "@heroicons/react/24/outline";
 import { modalState, postIdState } from "../atoms/modalAtom";
 import Moment from "react-moment";
 import { useRecoilState } from "recoil";
 import { useRouter } from "next/router";
 import { db } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { setDoc,deleteDoc, doc } from "firebase/firestore";
+import {onSnapshot, collection} from "@firebase/firestore"
 
 
 function Post({ id, post, postPage }) {
-  const { data: session } = useSession();
-  const [isOpen, setIsOpen] = useRecoilState(modalState);
-  const [postId, setPostId] = useRecoilState(postIdState);
-  const [comments, setComments] = useState([]);
-  const router = useRouter()
-  return (
-    <div className="p-3 flex cursor-pointer border-b border-gray-700"
-    onClick={()=> router.push(`/${id}`)}>
-      {!postPage && (
-        <img
-          src={post?.userImg}
-          alt="Profile pic"
-          className="h-11 w-11 rounded-full mr-4"
-        />
-      )}
-      <div className="flex flex-col space-y-2 w-full">
-        <div className={`flex ${!postPage && "justify-between"}`}>
-          {postPage && (
-            <img
-              src={post?.userImg}
-              alt="Profile pic"
-              className="h-11 w-11 rounded-full mr-4"
-            />
-          )}
-          <div className="text-[#6e767d]">
-            <div className="inline-block group">
-              <h4
-                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] group-hover:underline ${
-                  !postPage && "inline-block"
-                } `}
-              >
-                {post?.username}
-              </h4>
-              <span
-                className={`text-sm sm:text-[15px] ${!postPage && "ml-1.5"}`}
-              >
-                @{post?.tag}
-              </span>
-            </div>{" "}
-            ·{" "}
-            <span className="hover:underline text-sm sm:text-[15px]">
-              {/* k kura kati time agadi post garya yo moment le garya */}
-              {/* <Moment fromNow>{post?.timestamp?.toDate()}</Moment> */}
-            </span>
-            {/* words lai dynamic banako eta */}
+    const { data: session } = useSession();
+    const [isOpen, setIsOpen] = useRecoilState(modalState);
+    const [postId, setPostId] = useRecoilState(postIdState);
+    const [comments, setComments] = useState([]);
+    const router = useRouter();
+    const [liked, setLiked] = useState(false);
+    const [likes, setLikes] = useState([]);
+//Like ko part ko logic
+
+//setLiked initially useState ko empty array cha as shown in line 27 so teslai initialize garauna yo tala ko logic use garya
+    useEffect(
+        () =>
+            onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+                setLikes(snapshot.docs)
+            ),
+        [db, id]
+    );
+
+    useEffect(
+        () =>
+            setLiked(
+                //findIndex works similar like map. findIndex is for array. it has a predicate and a value
+                likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+            ),
+        [likes]
+    );
+
+    const likePost = async () => {
+        //edi already liked cha vane deletedocument hos i.e. unlike hos arko palta daabda 
+        if (liked) {
+            await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+        } else {
+            await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+                username: session.user.name,
+            });
+        }
+    };
+
+    //comment ko part ko logic
+    return (
+        <div className="p-3 flex cursor-pointer border-b border-gray-700"
+            onClick={() => router.push(`/${id}`)}>
             {!postPage && (
-              <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
-                {post?.text}
-              </p>
+                <img
+                    src={post?.userImg}
+                    alt="Profile pic"
+                    className="h-11 w-11 rounded-full mr-4"
+                />
             )}
-          </div>
-          <div className="icon group flex-shrink-0 ml-auto">
-            <EllipsisHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#ff7f11]" />
-          </div>
-        </div>
-        {postPage && (
-          <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
-            {post?.text}
-          </p>
-        )}
-        <img
-          src={post?.image}
-          alt=""
-          className="rounded-2xl max-h-[700px] object-cover mr-2"
-        />
-        <div
-          className={`text-[#6e767d] flex justify-between w-10/12 ${
-            postPage && "mx-auto"
-          }`}
-        >
-          <div
-            className="flex items-center space-x-1 group"
-            onClick={(e) => {
-              e.stopPropagation();
-              setPostId(id);
-              //setIsOpen vanya modalState ho ra true vaneko yo open cha indicate garya
-              setIsOpen(true);
-            }}
-          >
-            <div className="icon group-hover:bg-[#1d9bf0] group-hover:bg-opacity-10">
-              <ChatBubbleLeftRightIcon className="h-5 group-hover:text-[#1d9bf0]" />
-            </div>
-            {comments.length > 0 && (
-              <span className="group-hover:text-[#1d9bf0] text-sm">
-                {comments.length}
-              </span>
-            )}
-          </div>
+            <div className="flex flex-col space-y-2 w-full">
+                <div className={`flex ${!postPage && "justify-between"}`}>
+                    {postPage && (
+                        <img
+                            src={post?.userImg}
+                            alt="Profile pic"
+                            className="h-11 w-11 rounded-full mr-4"
+                        />
+                    )}
+                    <div className="text-[#6e767d]">
+                        <div className="inline-block group">
+                            <h4
+                                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] group-hover:underline ${!postPage && "inline-block"
+                                    } `}
+                            >
+                                {post?.username}
+                            </h4>
+                            <span
+                                className={`text-sm sm:text-[15px] ${!postPage && "ml-1.5"}`}
+                            >
+                                @{post?.tag}
+                            </span>
+                        </div>{" "}
+                        ·{" "}
+                        <span className="hover:underline text-sm sm:text-[15px]">
+                            {/* k kura kati time agadi post garya yo moment le garya */}
+                            <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
+                        </span>
+                        {/* words lai dynamic banako eta */}
+                        {!postPage && (
+                            <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
+                                {post?.text}
+                            </p>
+                        )}
+                    </div>
+                    <div className="icon group flex-shrink-0 ml-auto">
+                        <EllipsisHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#ff7f11]" />
+                    </div>
+                </div>
+                {postPage && (
+                    <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
+                        {post?.text}
+                    </p>
+                )}
+                <img
+                    src={post?.image}
+                    alt=""
+                    className="rounded-2xl max-h-[700px] object-cover mr-2"
+                />
+                <div
+                    className={`text-[#6e767d] flex justify-between w-10/12 ${postPage && "mx-auto"
+                        }`}
+                >
+                    <div
+                        className="flex items-center space-x-1 group"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            setPostId(id);
+                            //setIsOpen vanya modalState ho ra true vaneko yo open cha indicate garya
+                            setIsOpen(true);
+                        }}
+                    >
+                        <div className="icon group-hover:bg-[#1d9bf0] group-hover:bg-opacity-10">
+                            <ChatBubbleLeftRightIcon className="h-5 group-hover:text-[#1d9bf0]" />
+                        </div>
+                        {comments.length > 0 && (
+                            <span className="group-hover:text-[#1d9bf0] text-sm">
+                                {comments.length}
+                            </span>
+                        )}
+                    </div>
 
-          {session.user.uid === post?.id ? (
-            <div
-              className="flex items-center space-x-1 group"
-              onClick={(e) => {
-                e.stopPropagation();
-                deleteDoc(doc(db, "posts", id));
-                router.push("/");
-              }}
-            >
-              <div className="icon group-hover:bg-red-600/10">
-                <TrashIcon className="h-5 group-hover:text-red-600" />
-              </div>
-            </div>
-          ) : (
-            <div className="flex items-center space-x-1 group">
-              <div className="icon group-hover:bg-green-500/10">
-                <EllipsisHorizontalIcon className="h-5 group-hover:text-green-500" />
-              </div>
-            </div>
-          )}
+                    {session.user.uid === post?.id ? (
+                        <div
+                            className="flex items-center space-x-1 group"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                deleteDoc(doc(db, "posts", id));
+                                router.push("/");
+                            }}
+                        >
+                            <div className="icon group-hover:bg-red-600/10">
+                                <TrashIcon className="h-5 group-hover:text-red-600" />
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="flex items-center space-x-1 group">
+                            <div className="icon group-hover:bg-green-500/10">
+                                <EllipsisHorizontalIcon className="h-5 group-hover:text-green-500" />
+                            </div>
+                        </div>
+                    )}
 
-          {/* <div
+                    <div
                         className="flex items-center space-x-1 group"
                         onClick={(e) => {
                             e.stopPropagation();
@@ -135,9 +169,9 @@ function Post({ id, post, postPage }) {
                     >
                         <div className="icon group-hover:bg-pink-600/10">
                             {liked ? (
-                                <HeartIconFilled className="h-5 text-pink-600" />
+                                <GiftIcon className="h-5 text-[#ff7f11]" />
                             ) : (
-                                <HeartIcon className="h-5 group-hover:text-pink-600" />
+                                <GiftTopIcon className="h-5 group-hover:text-[#aa7242]" />
                             )}
                         </div>
                         {likes.length > 0 && (
@@ -148,18 +182,18 @@ function Post({ id, post, postPage }) {
                                 {likes.length}
                             </span>
                         )}
-                    </div> */}
+                    </div>
 
-          <div className="icon group">
-            <ShareIcon className="h-5 group-hover:text-[#1d9bf0]" />
-          </div>
-          <div className="icon group">
-            <ChartBarIcon className="h-5 group-hover:text-[#1d9bf0]" />
-          </div>
+                    <div className="icon group">
+                        <ShareIcon className="h-5 group-hover:text-[#1d9bf0]" />
+                    </div>
+                    <div className="icon group">
+                        <ChartBarIcon className="h-5 group-hover:text-[#1d9bf0]" />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Post;
