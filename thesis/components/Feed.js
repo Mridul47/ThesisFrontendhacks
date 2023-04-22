6842474127163a16ee47d24d9f049371cdbae8c7
Mridul Koirala <mrkoirala58@gmail.com>
2023-04-22 12:28:42 +0545

    Feed useeffect generated

--- thesis/components/Feed.js
+++ thesis/components/Feed.js
@@ -1,8 +1,41 @@
 import { SparklesIcon } from '@heroicons/react/24/outline'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Input from './Input'
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import { db } from '../firebase';
+// import Post from "./Post";
+import { useSession } from "next-auth/react";
 
 function Feed() {
+    const { data: session } = useSession();
+    const [posts, setPosts] = useState([]);
+
+    // MESSY code
+    // useEffect(() => {
+    //   const unsubscribe = onSnapshot(
+    //     query(collection(db, "posts"), orderBy("timestamp", "desc")),
+    //     (snapshot) => {
+    //       setPosts(snapshot.docs);
+    //     }
+    //   );
+
+    //   return () => {
+    //     unsubscribe();
+    //   };
+    // }, [db]);
+
+    // we can also do like this
+    useEffect(
+        () =>
+            onSnapshot(
+                //quering the post from database  in descending order
+                query(collection(db, "posts"), orderBy("timestamp", "desc")),
+                (snapshot) => {
+                    setPosts(snapshot.docs);
+                }
+            ),
+        [db]
+    );
     return (
         <div className='text-white flex-grow border-l border-r border-gray-700 max-w-screen-2xl sm:ml-[73px] xl:ml-[370px]'>
             <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
@@ -12,6 +45,9 @@ function Feed() {
                 </div>
             </div>
             <Input />
+            <div className="pb-72">
+                
+            </div>
         </div>
     )
 }
